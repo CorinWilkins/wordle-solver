54a9c08c821fc31442d41f1dc168cd00531b31fa
corin <corinlwilkins@gmail.com>
2022-05-05 00:33:29 +0100

    Improves UX

--- wordle/main.py
+++ wordle/main.py
@@ -4,21 +4,23 @@ import requests
 
 target_url = 'https://raw.githubusercontent.com/tabatkins/wordle-list/main/words'
 word_list = requests.get(target_url).text.split("\n")
+word_list.sort()
 
 @click.command()
 def find():
     guesses = []
     while True:
         length = len(guesses)
-        message = 'Please enter a guessed word' +  (', leave empty to return results' if length > 0 else '')
+        message = 'Please enter a guessed word' +  (', leave empty to exit' if length > 0 else '')
         guess = click.prompt(message, default='', type=str)
         if guess == '':
             if length > 0:
                 break
             else: continue
-
-
-        guesses.append(guess)
-        guesses.append(click.prompt('Please enter a guess\'s matches', type=str))
-
-    click.echo(get_matches_for_guesses(word_list, guesses))
+        else:
+            guesses.append(guess)
+            guesses.append(click.prompt('Please enter a guess\'s matches', type=str))
+            matches = get_matches_for_guesses(word_list, guesses)
+            
+            click.echo(matches)
+            click.echo(f'{len(matches)} possibilities')
