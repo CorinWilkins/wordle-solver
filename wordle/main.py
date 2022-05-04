b20dafd10c22e1c692ed57d22c09b60258e5be7b
corin <corinlwilkins@gmail.com>
2022-05-05 00:48:06 +0100

    Adds input restrictions

--- wordle/main.py
+++ wordle/main.py
@@ -1,3 +1,4 @@
+import re
 import click
 from wordle.wordle import get_matches_for_guesses
 import requests
@@ -12,15 +13,27 @@ def find():
     while True:
         length = len(guesses)
         message = 'Please enter a guessed word' +  (', leave empty to exit' if length > 0 else '')
-        guess = click.prompt(message, default='', type=str)
+        while True:
+            guess = click.prompt(message, default='', type=str).lower()
+            if len(guess) == 5 or len(guess) == 0:
+                break
+            click.echo('guess must contain 5 letters')
+
         if guess == '':
             if length > 0:
                 break
             else: continue
         else:
             guesses.append(guess)
-            guesses.append(click.prompt('Please enter a guess\'s matches', type=str))
+            while True:
+                match = click.prompt('Please enter a guess\'s matches 2=green, 1=yellow, 0=black', type=str)
+                if len(match) == 5 and not re.findall(r'([^012])', match):
+                    break
+                click.echo('match must contain 5 characters and only 0s, 1s, and 2s')
+
+
+            guesses.append(match)
             matches = get_matches_for_guesses(word_list, guesses)
             
-            click.echo(matches)
+            click.echo(matches.join('\n'))
             click.echo(f'{len(matches)} possibilities')
