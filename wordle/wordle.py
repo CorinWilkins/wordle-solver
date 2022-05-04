5727ffff608d1ed707fa56b59f09a97b996eb5cb
corin <corinlwilkins@gmail.com>
2022-05-04 21:41:25 +0100

    Cleans up refactored code.

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -41,37 +41,3 @@ def does_guess_match_word(guess, word, match):
         if char == '0' and count[word[position]] > 0:
             return False
     return True
-
-
-def get_exact_matches(words, word, match):
-    out = []
-    match_chars = [{'pos': pos, 'char': word[pos]} for pos, char in enumerate(match) if char == '2']
-    for w in words:
-        for pattern in match_chars:
-            if w[pattern['pos']] == pattern['char']:
-                out.append(w)
-                break
-    return out
-
-
-def get_negative_matches(words, word, match):
-    out = []
-    match_chars = [word[pos] for pos, char in enumerate(match) if char == '0']
-    for w in words:
-        if not any(c in w for c in match_chars):
-            out.append(w)
-    return out
-
-def get_fuzzy_matches(words, word, match):
-    print(words)
-    out = []
-    match_chars = [{'pos': pos, 'char': word[pos]} for pos, char in enumerate(match) if char == '1']
-    
-    for w in words:
-        for pattern in match_chars:
-            char = pattern['char']
-            print(char, w[pattern['pos']], w[pattern['pos']] != char,  char in w)
-            if (w[pattern['pos']] != char) and  char in w:
-                out.append(w)
-                break
-    return out
