d7c1fc8fe79f4ed4e49774bfd89fb8accfad02bd
corin <corinlwilkins@gmail.com>
2023-11-05 20:54:06 +0000

    refactors into usecase

--- wordle/main.py
+++ wordle/main.py
@@ -5,66 +5,28 @@ import re
 from wordle.wordle import build_word_matches_naive, get_matches_for_guesses, get_next_best_word_log_memoed, get_all_possible_words
 
 
-def validate_guess(ctx, param, value):
-    if isinstance(value, tuple):
-        return value
-
-    try:
-        rolls, _, dice = value.partition("d")
-        return int(dice), int(rolls)
-    except ValueError:
-        raise click.BadParameter("format must be 'NdM'")
-
-
 @click.command()
 def find():
-    all_words = get_all_possible_words()
-    memoed_matches = build_word_matches_naive(all_words)
-    guesses = []
+    
+    usecase = WordleSolver(Gateway(),Presenter())
     while True:
-        length = len(guesses)
         guess = Guess()
-        message = 'Please enter a guessed word' +  (', leave empty to exit' if length > 0 else '')
         while True:
-            guess.word = click.prompt(message, default='', type=str).lower()
-            if len(guess.word) == 5 or len(guess.word) == 0:
+            guess.word = click.prompt('Please enter a guessed word', default='', type=str).lower()
+            if len(guess.word) == 5:
                 break
             click.echo('guess must contain 5 letters')
 
-        if guess.word == '' or guess.word == 'exit':
-            if length > 0:
+        while True:
+            guess.pattern = click.prompt('Please enter a guess\'s matches 2=green, 1=yellow, 0=black', type=str)
+            if guess.pattern == '11111':
+                click.echo("Congratulations!")
+                return    
+            if len(guess.pattern) == 5 and not re.findall(r'([^012])', guess.pattern):
                 break
-            else: continue
-        else:
-            
-            while True:
-                guess.pattern = click.prompt('Please enter a guess\'s matches 2=green, 1=yellow, 0=black', type=str)
-                if guess.pattern == '11111':
-                    click.echo("Congratulations!")
-                    return    
-                if len(guess.pattern) == 5 and not re.findall(r'([^012])', guess.pattern):
-                    break
-                click.echo('pattern must contain 5 characters and only 0s, 1s, and 2s')
+            click.echo('pattern must contain 5 characters and only 0s, 1s, and 2s')
 
-
-            guesses.append(guess)
-            matches = get_matches_for_guesses(all_words, guesses)
-            present(matches, memoed_matches)
-
-
-def present(matches, memoed_matches):
-    num_possibilities = len(matches)
-    click.echo(f'{num_possibilities} possibilities')
-    if num_possibilities < 1:
-        click.echo('No remaining possiblities')
-    elif num_possibilities == 1:
-        click.echo(tabulate([(matches[0], 1)], headers=['Word', 'Mean Remaining Possiblities'], tablefmt="github"))
-    else:    
-        limit = 10
-        next_best_words = get_next_best_word_log_memoed(matches, memoed_matches, limit)
-        click.echo(tabulate(next_best_words, headers=['Word', 'Mean Remaining Possiblities'], tablefmt="github"))
-
-
+        result = usecase(guess, 10)
 
 class Guess:
     word: str
@@ -73,11 +35,30 @@ class Guess:
 class WordleSolver():
     guesses = []
 
-    def __init__(self, gateway) -> None:
-        self.all_words = self.gateway.get_all_words()
+    def __init__(self, gateway, presenter) -> None:
+        self.all_words = gateway.get_all_possible_words()
         self.memoed_matches = build_word_matches_naive(self.all_words)
+        self.presenter = presenter
 
-
-    def __call__(self, guess:Guess) -> Any:
+    def __call__(self, guess:Guess, limit) -> Any:
         self.guesses.append(guess)
         get_matches_for_guesses(self.all_words, self.guesses)
+
+        matches = get_matches_for_guesses(self.all_words, self.guesses)
+        next_best_words = get_next_best_word_log_memoed(matches, self.memoed_matches, limit)
+        self.presenter(matches, next_best_words)
+
+class Presenter:
+    def __call__(self, matches, next_best_words):
+        num_possibilities = len(matches)
+        click.echo(f'{num_possibilities} possibilities')
+        if num_possibilities < 1:
+            click.echo('No remaining possiblities')
+        elif num_possibilities == 1:
+            click.echo(tabulate(next_best_words, headers=['Word', 'Mean Remaining Possiblities'], tablefmt="github"))
+        else:    
+            click.echo(tabulate(next_best_words, headers=['Word', 'Mean Remaining Possiblities'], tablefmt="github"))
+
+class Gateway:
+    def get_all_possible_words(self):
+        return get_all_possible_words()
